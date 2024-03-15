f2973468ac19730511d87abc32c020cc23da3e08
unknown <dharanikumthi@gmail.com>
2024-03-15 20:13:54 +0100

    Classifier to add predicated values

--- source/Project/MySolution/MySolution/MultiSequenceLearning.cs
+++ source/Project/MySolution/MySolution/MultiSequenceLearning.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace NeoCortexApiSample
 {
@@ -19,7 +18,7 @@ namespace NeoCortexApiSample
         /// <summary>
         /// Runs the learning of sequences.
         /// </summary>
-        /// <param name="sequences">Dicti
+        /// <param name="sequences">Dictionary of sequences. KEY is the sequence name, the VALUE is the list of elements of the sequence.</param>
         public Predictor Run(List<double> inputValues)
         {
             Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(MultiSequenceLearning)}");
@@ -54,7 +53,6 @@ namespace NeoCortexApiSample
                 PredictedSegmentDecrement = 0.1
             };
 
-            //double max = 20;
             double max = 255;
             Dictionary<string, object> settings = new Dictionary<string, object>()
             {
@@ -89,108 +87,74 @@ namespace NeoCortexApiSample
 
             HtmClassifier<string, ComputeCycle> cls = new HtmClassifier<string, ComputeCycle>();
 
-            double totalLoss = 0.0; // To keep track of the total binary cross-entropy loss
-
             var numUniqueInputs = inputValues.Distinct<double>().ToList().Count;
 
             CortexLayer<object, object> layer1 = new CortexLayer<object, object>("L1");
 
             TemporalMemory tm = new TemporalMemory();
 
-            // For more information see following paper: https://www.scitepress.org/Papers/2021/103142/103142.pdf
+            // For more information see the following paper: https://www.scitepress.org/Papers/2021/103142/103142.pdf
             HomeostaticPlasticityController hpc = new HomeostaticPlasticityController(mem, numUniqueInputs * 150, (isStable, numPatterns, actColAvg, seenInputs) =>
             {
                 if (isStable)
                     // Event should be fired when entering the stable state.
                     Debug.WriteLine($"STABLE: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
                 else
-                    // Ideal SP should never enter unstable state after stable state.
+                    // Ideal SP should never enter unstable state after the stable state.
                     Debug.WriteLine($"INSTABLE: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
 
-                // We are not learning in instable state.
+                // We are not learning in the unstable state.
                 isInStableState = isStable;
 
                 // Clear active and predictive cells.
-                //tm.Reset(mem);
+                // tm.Reset(mem);
             }, numOfCyclesToWaitOnChange: 50);
-
 
             SpatialPoolerMT sp = new SpatialPoolerMT(hpc);
             sp.Init(mem);
             tm.Init(mem);
 
-            // Please note that we do not add here TM in the layer.
-            // This is omitted for practical reasons, because we first eneter the newborn-stage of the algorithm
-            // In this stage we want that SP get boosted and see all elements before we start learning with TM.
-            // All would also work fine with TM in layer, but it would work much slower.
-            // So, to improve the speed of experiment, we first ommit the TM and then after the newborn-stage we add it to the layer.
             layer1.HtmModules.Add("encoder", encoder);
             layer1.HtmModules.Add("sp", sp);
 
             double[] inputs = inputValues.ToArray();
-            int[] prevActiveCols = new int[0];
-
             int cycle = 0;
             int matches = 0;
-
-            //string lastPredictedValue = "0";
             var lastPredictedValues = new List<string>(new string[] { "0" });
-
-            // var lastPredictedValues = "0";
             int maxCycles = 3500;
 
-            //
-            // Training SP to get stable. New-born stage.
-            //
-
-            for (int i = 0; i < maxCycles && isInStableState == false; i++)
+            // Training SP to stabilize. Newborn stage.
+            for (int i = 0; i < maxCycles && !isInStableState; i++)
             {
                 matches = 0;
-
                 cycle++;
 
                 Debug.WriteLine($"-------------- Newborn Cycle {cycle} ---------------");
+
                 foreach (var input in inputs)
                 {
                     Debug.WriteLine($" -- {input} --");
-
                     var lyrOut = layer1.Compute(input, true);
-
                     if (isInStableState)
                         break;
                 }
 
                 if (isInStableState)
                     break;
-
             }
 
-            // Clear all learned patterns in the classifier.
             cls.ClearState();
-
-            // We activate here the Temporal Memory algorithm.
             layer1.HtmModules.Add("tm", tm);
 
-
             int maxPrevInputs = inputValues.Count - 1;
-
-            List<string> previousInputs = new List<string>();
-
-            previousInputs.Add("-1.0");
-
-            // Set on true if the system has learned the sequence with a maximum acurracy.
+            List<string> previousInputs = new List<string> { "-1.0" };
             bool isLearningCompleted = false;
-
-            //
-            // Now training with SP+TM. SP is pretrained on the given input pattern.
-
 
             foreach (var input1 in inputs)
             {
                 for (int i = 0; i < maxCycles; i++)
                 {
                     matches = 0;
-
                     cycle++;
 
                     Debug.WriteLine($"-------------- Cycle {cycle} ---------------");
@@ -199,10 +163,7 @@ namespace NeoCortexApiSample
                     {
                         Debug.WriteLine($"-------------- {input} ---------------");
 
-                        // lyrOut is null when the TM is added to the layer inside of HPC callback by entering of the stable state.
-
                         var lyrOut = layer1.Compute(input, true) as ComputeCycle;
-                        // if (isInStableState && lyrOut != null)
 
                         var activeColumns = layer1.GetResult("sp") as int[];
 
@@ -210,40 +171,19 @@ namespace NeoCortexApiSample
                         if (previousInputs.Count > (maxPrevInputs + 1))
                             previousInputs.RemoveAt(0);
 
-                        // In the pretrained SP with HPC, the TM will quickly learn cells for patterns
-                        // In that case the starting sequence 4-5-6 might have the sam SDR as 1-2-3-4-5-6,
-                        // Which will result in returning of 4-5-6 instead of 1-2-3-4-5-6.
-                        // HtmClassifier allways return the first matching sequence. Because 4-5-6 will be as first
-                        // memorized, it will match as the first one.
                         if (previousInputs.Count < maxPrevInputs)
-
                             continue;
-                        Console.WriteLine($"maxPrevInput : {maxPrevInputs}");
-                        Console.WriteLine($"previousInputs.Count : {previousInputs.Count}");
 
                         string key = GetKey(previousInputs, input);
-                        Console.WriteLine($"key = {key}");
 
-                        List<Cell> actCells;
-
-                        if (lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count)
-                        {
-                            actCells = lyrOut.ActiveCells;
-                        }
-                        else
-                        {
-                            actCells = lyrOut.WinnerCells;
-                        }
+                        List<Cell> actCells = lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count
+                            ? lyrOut.ActiveCells
+                            : lyrOut.WinnerCells;
 
                         cls.Learn(key, actCells.ToArray());
 
                         Debug.WriteLine($"Col  SDR: {Helpers.StringifyVector(lyrOut.ActivColumnIndicies)}");
                         Debug.WriteLine($"Cell SDR: {Helpers.StringifyVector(actCells.Select(c => c.Index).ToArray())}");
-
-                        //
-                        // If the list of predicted values from the previous step contains the currently presenting value,
-                        // we have a match.
-
 
                         if (lastPredictedValues.Contains(key))
                         {
@@ -251,53 +191,36 @@ namespace NeoCortexApiSample
                             Debug.WriteLine($"Match. Actual value: {key} - Predicted value: {lastPredictedValues.FirstOrDefault(key)}.");
                         }
                         else
-                            Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
-
-                        // Calculate binary cross-entropy loss
-                        double binaryCrossEntropyLoss = CalculateBinaryCrossEntropyLoss(cls, key, input);
-                        totalLoss += binaryCrossEntropyLoss;
+                            Debug.WriteLine($"Mismatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
 
                         if (lyrOut.PredictiveCells.Count > 0)
                         {
-                            //var predictedInputValue = cls.GetPredictedInputValue(lyrOut.PredictiveCells.ToArray());
                             var predictedInputValues = cls.GetPredictedInputValues(lyrOut.PredictiveCells.ToArray(), 3);
                             Console.WriteLine($"predictedInputValues : {predictedInputValues}");
-
                             foreach (var item in predictedInputValues)
                             {
                                 Debug.WriteLine($"Current Input: {input} \t| Predicted Input: {item.PredictedInput} - {item.Similarity}");
                             }
 
                             lastPredictedValues = predictedInputValues.Select(v => v.PredictedInput).ToList();
-
                             Console.WriteLine($"The lastpredicted values : {lastPredictedValues}");
-
                         }
                         else
                         {
                             Debug.WriteLine($"NO CELLS PREDICTED for next cycle.");
                             lastPredictedValues = new List<string>();
                         }
-
                     }
 
-                    double maxPossibleAccuraccy = (double)((double)inputValues.Count() - 1) / (double)inputValues.Count() * 100.0;
-
-                    double accuracy = (double)matches / (double)inputValues.Count() * 100.0;
-
-
+                    double maxPossibleAccuracy = ((double)inputValues.Count - 1) / inputValues.Count * 100.0;
+                    double accuracy = matches / (double)inputValues.Count * 100.0;
                     Debug.WriteLine($"Cycle: {cycle}\tMatches={matches} of {inputValues.Count()}\t {accuracy}%");
 
-
-                    double averageLoss = totalLoss / inputValues.Count;
-                    Debug.WriteLine($"Average Binary Cross-Entropy Loss: {averageLoss}");
-                    if (accuracy >= maxPossibleAccuraccy)
+                    if (accuracy >= maxPossibleAccuracy)
                     {
                         maxMatchCnt++;
-                        Debug.WriteLine($"100% accuracy reched {maxMatchCnt} times.");
+                        Debug.WriteLine($"100% accuracy reached {maxMatchCnt} times.");
 
-                        //
-                        // Experiment is completed if we are 30 cycles long at the 100% accuracy.
                         if (maxMatchCnt >= 30)
                         {
                             sw.Stop();
@@ -308,17 +231,15 @@ namespace NeoCortexApiSample
                     }
                     else if (maxMatchCnt > 0)
                     {
-                        Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with accuracy {accuracy}. This indicates instable state. Learning will be continued.");
+                        Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with accuracy {accuracy}. This indicates unstable state. Learning will be continued.");
                         maxMatchCnt = 0;
                     }
 
-                    // This resets the learned state, so the first element starts allways from the beginning.
                     tm.Reset(mem);
-
-
                 }
-                if (isLearningCompleted == false)
-                    throw new Exception($"The system didn't learn with expected acurracy!");
+
+                if (!isLearningCompleted)
+                    throw new Exception($"The system didn't learn with expected accuracy!");
             }
 
             Debug.WriteLine("------------ END ------------");
@@ -326,21 +247,14 @@ namespace NeoCortexApiSample
             return new Predictor(layer1, mem, cls);
         }
 
-        private double CalculateBinaryCrossEntropyLoss(HtmClassifier<string, ComputeCycle> classifier, string target, double input)
-        {
-            double loss = 0.0;
-
-            // Retrieve the predicted probabilities from the classifier
-            double[] probabilities = classifier.();
-
-            // Assuming target is a binary value (0 or 1)
-            int targetValue = Convert.ToInt32(target);
-
-            // Calculate binary cross-entropy loss
-            loss = -((targetValue * Math.Log(probabilities[1] + double.Epsilon)) + ((1 - targetValue) * Math.Log(probabilities[0] + double.Epsilon)));
-
-            return loss;
-        }
+        /// <summary>
+        /// Constructs the unique key of the element of a sequence. This key is used as input for HtmClassifier.
+        /// It makes sure that all elements that belong to the same sequence are prefixed with the sequence.
+        /// The prediction code can then extract the sequence prefix to the predicted element.
+        /// </summary>
+        /// <param name="prevInputs"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
         private static string GetKey(List<string> prevInputs, double input)
         {
             string key = String.Empty;
@@ -350,19 +264,26 @@ namespace NeoCortexApiSample
                 if (i > 0)
                     key += "-";
 
-                key += (prevInputs[i]);
+                key += prevInputs[i];
             }
 
             return key;
         }
+
+        private double CalculateBinaryCrossEntropyLoss(HtmClassifier<string, ComputeCycle> classifier, string target, double input)
+        {
+            double loss = 0.0;
+
+            // Retrieve the predicted probabilities from the classifier
+            double[] probabilities = classifier.GetPredictedProbabilities(target);
+
+            // Assuming target is a binary value (0 or 1)
+            int targetValue = Convert.ToInt32(target);
+
+            // Calculate binary cross-entropy loss
+            loss = -((targetValue * Math.Log(probabilities[1] + double.Epsilon)) + ((1 - targetValue) * Math.Log(probabilities[0] + double.Epsilon)));
+
+            return loss;
+        }
     }
-
-
-
-
-
 }
-
-
-
-
