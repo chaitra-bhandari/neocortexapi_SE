c8730addd6536f746fb335f23d57f5e23d4e9ee6
unknown <dharanikumthi@gmail.com>
2024-03-16 23:59:57 +0100

    Setting Threshold value

--- source/Project/MySolution/MySolution/MultiSequenceLearning.cs
+++ source/Project/MySolution/MySolution/MultiSequenceLearning.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
+
 namespace NeoCortexApiSample
 {
     /// <summary>
@@ -18,7 +19,7 @@ namespace NeoCortexApiSample
         /// <summary>
         /// Runs the learning of sequences.
         /// </summary>
-        /// <param name="sequences">Dictionary of sequences. KEY is the sequence name, the VALUE is the list of elements of the sequence.</param>
+        /// <param name="sequences">Dictionary of sequences. KEY is the sewuence name, the VALUE is th elist of element of the sequence.</param>
         public Predictor Run(List<double> inputValues)
         {
             Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(MultiSequenceLearning)}");
@@ -53,6 +54,7 @@ namespace NeoCortexApiSample
                 PredictedSegmentDecrement = 0.1
             };
 
+            //double max = 20;
             double max = 255;
             Dictionary<string, object> settings = new Dictionary<string, object>()
             {
@@ -93,77 +95,115 @@ namespace NeoCortexApiSample
 
             TemporalMemory tm = new TemporalMemory();
 
-            // For more information see the following paper: https://www.scitepress.org/Papers/2021/103142/103142.pdf
+            // For more information see following paper: https://www.scitepress.org/Papers/2021/103142/103142.pdf
             HomeostaticPlasticityController hpc = new HomeostaticPlasticityController(mem, numUniqueInputs * 150, (isStable, numPatterns, actColAvg, seenInputs) =>
             {
                 if (isStable)
                     // Event should be fired when entering the stable state.
                     Debug.WriteLine($"STABLE: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
                 else
-                    // Ideal SP should never enter unstable state after the stable state.
+                    // Ideal SP should never enter unstable state after stable state.
                     Debug.WriteLine($"INSTABLE: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
 
-                // We are not learning in the unstable state.
+                // We are not learning in instable state.
                 isInStableState = isStable;
 
                 // Clear active and predictive cells.
-                // tm.Reset(mem);
+                //tm.Reset(mem);
             }, numOfCyclesToWaitOnChange: 50);
+
 
             SpatialPoolerMT sp = new SpatialPoolerMT(hpc);
             sp.Init(mem);
             tm.Init(mem);
 
+            // Please note that we do not add here TM in the layer.
+            // This is omitted for practical reasons, because we first eneter the newborn-stage of the algorithm
+            // In this stage we want that SP get boosted and see all elements before we start learning with TM.
+            // All would also work fine with TM in layer, but it would work much slower.
+            // So, to improve the speed of experiment, we first ommit the TM and then after the newborn-stage we add it to the layer.
             layer1.HtmModules.Add("encoder", encoder);
             layer1.HtmModules.Add("sp", sp);
 
             double[] inputs = inputValues.ToArray();
+            int[] prevActiveCols = new int[0];
+
             int cycle = 0;
             int matches = 0;
+
+            //string lastPredictedValue = "0";
             var lastPredictedValues = new List<string>(new string[] { "0" });
+
+            // var lastPredictedValues = "0";
             int maxCycles = 3500;
 
-            // Training SP to stabilize. Newborn stage.
-            for (int i = 0; i < maxCycles && !isInStableState; i++)
+            //
+            // Training SP to get stable. New-born stage.
+            //
+
+            for (int i = 0; i < maxCycles && isInStableState == false; i++)
             {
                 matches = 0;
+
                 cycle++;
 
                 Debug.WriteLine($"-------------- Newborn Cycle {cycle} ---------------");
 
+
                 foreach (var input in inputs)
                 {
                     Debug.WriteLine($" -- {input} --");
+
                     var lyrOut = layer1.Compute(input, true);
+
                     if (isInStableState)
                         break;
                 }
 
                 if (isInStableState)
                     break;
+
             }
 
+            // Clear all learned patterns in the classifier.
             cls.ClearState();
+
+            // We activate here the Temporal Memory algorithm.
             layer1.HtmModules.Add("tm", tm);
 
+
             int maxPrevInputs = inputValues.Count - 1;
-            List<string> previousInputs = new List<string> { "-1.0" };
+
+            List<string> previousInputs = new List<string>();
+
+            previousInputs.Add("-1.0");
+
+            // Set on true if the system has learned the sequence with a maximum acurracy.
             bool isLearningCompleted = false;
 
+            //
+            // Now training with SP+TM. SP is pretrained on the given input pattern set.
             foreach (var input1 in inputs)
             {
                 for (int i = 0; i < maxCycles; i++)
                 {
                     matches = 0;
+
                     cycle++;
 
+
+
                     Debug.WriteLine($"-------------- Cycle {cycle} ---------------");
+
 
                     foreach (var input in inputs)
                     {
                         Debug.WriteLine($"-------------- {input} ---------------");
 
+                        // lyrOut is null when the TM is added to the layer inside of HPC callback by entering of the stable state.
+
                         var lyrOut = layer1.Compute(input, true) as ComputeCycle;
+                        // if (isInStableState && lyrOut != null)
 
                         var activeColumns = layer1.GetResult("sp") as int[];
 
@@ -171,19 +211,68 @@ namespace NeoCortexApiSample
                         if (previousInputs.Count > (maxPrevInputs + 1))
                             previousInputs.RemoveAt(0);
 
+                        // In the pretrained SP with HPC, the TM will quickly learn cells for patterns
+                        // In that case the starting sequence 4-5-6 might have the sam SDR as 1-2-3-4-5-6,
+                        // Which will result in returning of 4-5-6 instead of 1-2-3-4-5-6.
+                        // HtmClassifier allways return the first matching sequence. Because 4-5-6 will be as first
+                        // memorized, it will match as the first one.
                         if (previousInputs.Count < maxPrevInputs)
+
                             continue;
+                        Console.WriteLine($"maxPrevInput : {maxPrevInputs}");
+                        Console.WriteLine($"previousInputs.Count : {previousInputs.Count}");
 
                         string key = GetKey(previousInputs, input);
+                        Console.WriteLine($"key = {key}");
 
-                        List<Cell> actCells = lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count
-                            ? lyrOut.ActiveCells
-                            : lyrOut.WinnerCells;
+                        List<Cell> actCells;
+
+                        if (lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count)
+                        {
+                            actCells = lyrOut.ActiveCells;
+                        }
+                        else
+                        {
+                            actCells = lyrOut.WinnerCells;
+                        }
 
                         cls.Learn(key, actCells.ToArray());
 
                         Debug.WriteLine($"Col  SDR: {Helpers.StringifyVector(lyrOut.ActivColumnIndicies)}");
                         Debug.WriteLine($"Cell SDR: {Helpers.StringifyVector(actCells.Select(c => c.Index).ToArray())}");
+                        String SdrValues = Helpers.StringifyVector(actCells.Select(c => c.Index).ToArray());
+                        // Split the input string by commas and remove any leading or trailing whitespace from each substring
+                        string[] substrings = SdrValues.Split(',')
+                                                       .Select(s => s.Trim())
+                                                       .ToArray();
+
+                        // Convert each substring to an integer and add it to a list
+                        List<int> intList = new List<int>();
+                        foreach (var substring in substrings)
+                        {
+                            if (int.TryParse(substring, out int intValue))
+                            {
+                                intList.Add(intValue);
+                            }
+                            else
+                            {
+                                // Console.WriteLine($"Error: Unable to parse '{substring}' as an integer.");
+                                // Handle the error or continue with other substrings
+                            }
+                        }
+
+                        //Console.Write($"intList of SDR values is ");
+                        //foreach ( var item in intList)
+                        //    {
+                        //    Console.Write(item + " ");
+
+                        //    }
+                        //
+
+
+                        // If the list of predicted values from the previous step contains the currently presenting value,
+                        // we have a match.
+
 
                         if (lastPredictedValues.Contains(key))
                         {
@@ -191,36 +280,107 @@ namespace NeoCortexApiSample
                             Debug.WriteLine($"Match. Actual value: {key} - Predicted value: {lastPredictedValues.FirstOrDefault(key)}.");
                         }
                         else
-                            Debug.WriteLine($"Mismatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
+                            Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
 
                         if (lyrOut.PredictiveCells.Count > 0)
                         {
-                            var predictedInputValues = cls.GetPredictedInputValues(lyrOut.PredictiveCells.ToArray(), 3);
+                            //var predictedInputValue = cls.GetPredictedInputValue(lyrOut.PredictiveCells.ToArray());
+                            var predictedInputValues = cls.GetPredictedInputValues(lyrOut.PredictiveCells.ToArray(), 7);
                             Console.WriteLine($"predictedInputValues : {predictedInputValues}");
+
                             foreach (var item in predictedInputValues)
                             {
-                                Debug.WriteLine($"Current Input: {input} \t| Predicted Input: {item.PredictedInput} - {item.Similarity}");
+                                Debug.WriteLine($"Current Input: {input} \t| Predicted Input: {item.PredictedInput} - {item.Similarity}  ");
+
+
+
+                                String predictive_cells = item.BestMatchString;
+
+                                //Console.WriteLine($"predictive_cells : {item.BestMatchString}");
+
+                                string[] substrings1 = predictive_cells.Split(',')
+                                                       .Select(s => s.Trim())
+                                                       .ToArray();
+
+                                // Convert each substring to an integer and add it to a list
+                                List<int> intList1 = new List<int>();
+
+                                foreach (var substring in substrings1)
+                                {
+                                    if (int.TryParse(substring, out int intValue))
+                                    {
+                                        intList1.Add(intValue);
+                                    }
+                                    else
+                                    {
+                                        //  Console.WriteLine($"Error: Unable to parse '{substring}' as an integer.");
+                                        // Handle the error or continue with other substrings
+                                    }
+                                }
+                                List<List<int>> predictedSets = new List<List<int>> { intList1 };
+
+                                // predictedSets.Add(intList1);
+                                int threshold = 50; // Adjust threshold as needed
+
+                                for (int j = 0; j < predictedSets.Count; j++)
+                                {
+                                    double bce = CalculateBinaryCrossEntropy(intList, predictedSets[j], threshold);
+
+                                    Console.WriteLine($"Binary Cross-Entropy for Set {j + 1}: {bce}");
+                                }
+
                             }
 
                             lastPredictedValues = predictedInputValues.Select(v => v.PredictedInput).ToList();
+
                             Console.WriteLine($"The lastpredicted values : {lastPredictedValues}");
+
                         }
                         else
                         {
                             Debug.WriteLine($"NO CELLS PREDICTED for next cycle.");
                             lastPredictedValues = new List<string>();
                         }
+
                     }
 
-                    double maxPossibleAccuracy = ((double)inputValues.Count - 1) / inputValues.Count * 100.0;
-                    double accuracy = matches / (double)inputValues.Count * 100.0;
+                    // Calculate binary cross entropy
+                    static double CalculateBinaryCrossEntropy(List<int> actualOutputs, List<int> predictedValues, int threshold)
+                    {
+                        // Determine correctness based on threshold
+                        var correctness = actualOutputs.Zip(predictedValues, (actual, pred) => Math.Abs(actual - pred) <= threshold ? 1 : 0).ToList();
+
+                        // Compute binary cross-entropy
+                        double bce = CalculateBinaryCrossEntropy1(correctness);
+                        return bce;
+                    }
+
+                    static double CalculateBinaryCrossEntropy1(List<int> correctness)
+                    {
+                        // Compute binary cross-entropy
+                        double bce = 0;
+                        foreach (var c in correctness)
+                        {
+                            bce += c == 1 ? 0 : 1;
+                        }
+                        return bce / correctness.Count;
+                    }
+
+                    // The first element (a single element) in the sequence cannot be predicted
+                    double maxPossibleAccuraccy = (double)((double)inputValues.Count() - 1) / (double)inputValues.Count() * 100.0;
+
+                    double accuracy = (double)matches / (double)inputValues.Count() * 100.0;
+
+
                     Debug.WriteLine($"Cycle: {cycle}\tMatches={matches} of {inputValues.Count()}\t {accuracy}%");
 
-                    if (accuracy >= maxPossibleAccuracy)
+                    if (accuracy >= maxPossibleAccuraccy)
                     {
                         maxMatchCnt++;
-                        Debug.WriteLine($"100% accuracy reached {maxMatchCnt} times.");
+                        Debug.WriteLine($"100% accuracy reched {maxMatchCnt} times.");
 
+                        //
+                        // Experiment is completed if we are 30 cycles long at the 100% accuracy.
                         if (maxMatchCnt >= 30)
                         {
                             sw.Stop();
@@ -231,15 +391,17 @@ namespace NeoCortexApiSample
                     }
                     else if (maxMatchCnt > 0)
                     {
-                        Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with accuracy {accuracy}. This indicates unstable state. Learning will be continued.");
+                        Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with accuracy {accuracy}. This indicates instable state. Learning will be continued.");
                         maxMatchCnt = 0;
                     }
 
+                    // This resets the learned state, so the first element starts allways from the beginning.
                     tm.Reset(mem);
                 }
 
-                if (!isLearningCompleted)
-                    throw new Exception($"The system didn't learn with expected accuracy!");
+                if (isLearningCompleted == false)
+                    throw new Exception($"The system didn't learn with expected acurracy!");
+
             }
 
             Debug.WriteLine("------------ END ------------");
@@ -248,12 +410,13 @@ namespace NeoCortexApiSample
         }
 
         /// <summary>
-        /// Constructs the unique key of the element of a sequence. This key is used as input for HtmClassifier.
-        /// It makes sure that all elements that belong to the same sequence are prefixed with the sequence.
+        /// Constracts the unique key of the element of an sequece. This key is used as input for HtmClassifier.
+        /// It makes sure that alle elements that belong to the same sequence are prefixed with the sequence.
         /// The prediction code can then extract the sequence prefix to the predicted element.
         /// </summary>
         /// <param name="prevInputs"></param>
         /// <param name="input"></param>
+        /// <param name="sequence"></param>
         /// <returns></returns>
         private static string GetKey(List<string> prevInputs, double input)
         {
@@ -264,26 +427,10 @@ namespace NeoCortexApiSample
                 if (i > 0)
                     key += "-";
 
-                key += prevInputs[i];
+                key += (prevInputs[i]);
             }
 
             return key;
-        }
-
-        private double CalculateBinaryCrossEntropyLoss(HtmClassifier<string, ComputeCycle> classifier, string target, double input)
-        {
-            double loss = 0.0;
-
-            // Retrieve the predicted probabilities from the classifier
-            double[] probabilities = classifier.GetPredictedProbabilities(target);
-
-            // Assuming target is a binary value (0 or 1)
-            int targetValue = Convert.ToInt32(target);
-
-            // Calculate binary cross-entropy loss
-            loss = -((targetValue * Math.Log(probabilities[1] + double.Epsilon)) + ((1 - targetValue) * Math.Log(probabilities[0] + double.Epsilon)));
-
-            return loss;
         }
     }
 }
--- source/Project/MySolution/MySolution/Program.cs
+++ source/Project/MySolution/MySolution/Program.cs
@@ -5,6 +5,8 @@ using System.Diagnostics;
 using System.Text;
 using System.Collections.Generic;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections;
+using System.Numerics;
 //using static NeoCortexApi.Utility.GroupBy2<R>;
 
 namespace NeoCortexApiSample
@@ -12,13 +14,10 @@ namespace NeoCortexApiSample
     class Program
 
     {
-
-
         static void Main(string[] args)
         {
             RunMultiSequenceLearningExperiment();
         }
-
         /// <summary>
         /// This example demonstrates how to learn sequences and how to use the prediction mechanism.
         /// First,string is converted into an array of characters, and asciii value of each character is stored in a list.
@@ -28,8 +27,8 @@ namespace NeoCortexApiSample
         /// </summary>
         private static void RunMultiSequenceLearningExperiment()
         {
-            List<double> inputValues = new List<double>();
 
+            List<double> inputValues = new List<double>();
 
             //Path to the input text file.
             string filePath = @"filename.txt";
@@ -46,16 +45,6 @@ namespace NeoCortexApiSample
 
             }
 
-            //Print the ascii value
-            Console.WriteLine("ASCII Sequence:");
-            foreach (var item in inputValues)
-            {
-                Console.Write(item + " ");
-            }
-
-
-            //Call a method to devide the inpit values as batches
-
             //Define block size.
             int block_size = 8;
 
@@ -63,75 +52,36 @@ namespace NeoCortexApiSample
             int batch_size = 4;
 
             List<double> x = GetBatch(inputValues, block_size, batch_size);
-            foreach (var item in x)
-            {
-                Console.Write(item + " ");
-            }
 
-
-
-
-
-            //Prototype for building the prediction engine.}
+            //Prototype for building the prediction engine.
             MultiSequenceLearning experiment = new MultiSequenceLearning();
 
+            for (int i = 0; i < 24; i += block_size)
+
             {
+                List<double> chunk = x.GetRange(i, block_size);
 
+                var predictor = experiment.Run(chunk);
 
+                Console.Write("Ask Question: ");
 
+                // Read the user's input from the console
+                string inputText = Console.ReadLine();
 
+                List<double> asciiVal = new List<double>();
 
-
-                for (int i = 0; i < x.Count; i += block_size)
+                foreach (char c in inputText)
                 {
-                    List<double> chunk = x.GetRange(i, block_size);
 
-
-                    Console.WriteLine("Batch:");
-                    foreach (var item in chunk)
-                    {
-                        Console.Write(item + " ");
-                    }
-
-
-                    var predictor = experiment.Run(chunk);
-                    int n = x.Count;
-                    if (n < 32)
-                    {
-                        continue;
-                    }
-
-
-
-                    Console.Write("Ask Question: ");
-
-                    // Read the user's input from the console
-                    string inputText = Console.ReadLine();
-
-                    List<double> asciiVal = new List<double>();
-
-                    foreach (char c in inputText)
-                    {
-                        asciiVal.Add((double)c);
-                    }
-
-
-
-                    //Generate chatGPT model
-                    PredictNextElement(predictor, asciiVal);
-
+                    asciiVal.Add(c);
 
                 }
 
-
-
+                //Generate chatGPT model
+                predictor.Reset();
+                PredictNextElement(predictor, asciiVal);
             }
-
-
-
-
         }
-
 
         public static void PredictNextElement(Predictor predictor, List<double> myList)
         {
@@ -139,16 +89,12 @@ namespace NeoCortexApiSample
 
             foreach (var item in myList)
             {
-                //list <double> res = predictor.Predict(asciiValues);
+
                 var res = predictor.Predict(item);
                 if (item == myList.First())
                 {
                     if (res.Count > 0)
                     {
-                        //foreach (var pred in res)
-                        //    {
-                        //    Debug.WriteLine($"{pred.PredictedInput} - {pred.Similarity}");
-                        //    }
 
                         var tokens = res.First().PredictedInput.Split('_');
 
@@ -159,12 +105,8 @@ namespace NeoCortexApiSample
                         var tokens3 = tokens2.Last();
 
                         Debug.WriteLine($"Predicted Sequence: {tokens[0]}, predicted next element {tokens3.Last()}");
-                        Console.WriteLine("logLoss");
 
-                        //List<double> responseSequence = new List<double>(asciiValues);
-
-                        //responseSequence.Add(tokens3.Last());
-
+                        //Split a string into an array of substrings
                         string[] parts = tokens[0].Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
 
                         // Create a list to store the double values
@@ -176,9 +118,6 @@ namespace NeoCortexApiSample
                             double value = double.Parse(part);
                             doubleList.Add(value);
                         }
-                        //List<double> responseSequence = new List<double>(Array.ConvertAll(tokens[0].Split(' - '), double.Parse));
-
-
                         string generatedResponse = DecodeNumericalSequence(doubleList);
 
                         Console.WriteLine("Generated Response: " + generatedResponse); ;
@@ -187,13 +126,12 @@ namespace NeoCortexApiSample
                 else
                     Debug.WriteLine("Nothing predicted :( ");
                 //}
-
-
                 Debug.WriteLine("------------------------------");
 
                 static string DecodeNumericalSequence(List<double> generatedTokens)
                 {
                     // Decode generated tokens to characters
+
                     string decodedString = "";
                     foreach (int token in generatedTokens)
                     {
@@ -203,8 +141,10 @@ namespace NeoCortexApiSample
                 }
             }
         }
+
         //Function to read the file and return  char array.
         public static List<char> ReadFileAndConvertToCharList(string filePath)
+
         {
             List<char> charList = new List<char>();
 
@@ -262,10 +202,9 @@ namespace NeoCortexApiSample
                 }
 
             }
+
             Console.WriteLine($"totalDataSize: {totalDataSize}");
             int dataSize = data.Count;
-            Console.WriteLine($"DataSize: {dataSize}");
-            Console.WriteLine($"block_size: {block_size}");
             List<double> x = new List<double>();
 
             for (int i = 0; i < batch_size; i++)
@@ -279,12 +218,22 @@ namespace NeoCortexApiSample
 
             return x;
         }
-
-
-
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
